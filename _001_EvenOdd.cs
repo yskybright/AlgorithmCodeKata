8e719b91a0f7cb45fcba2ba9da5570f2fed3d791
yskybright <rgh99@naver.com>
2023-11-17 09:52:35 +0900

    Initial commit

---
+++ _001_EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    // 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+    // num은 int 범위의 정수입니다.
+    // 0은 짝수입니다.
+
+    internal class _001_EvenOdd
+    {
+        public string solution(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return ("Even");
+            }
+            else
+            {
+                return ("Odd");
+            }
+        }
+    }
+}
