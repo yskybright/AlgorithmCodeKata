99739a33b8c5e10ebfe274f7fb095e2b8cba46e0
yskybright <rgh99@naver.com>
2023-11-30 11:10:57 +0900

    23.11.30

---
+++ _020_DescendLong.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _020_DescendLong
+    {
+        public long solution(long n)
+        {
+            char[] a = n.ToString().ToCharArray();
+            Array.Sort(a);
+            Array.Reverse(a);
+
+            long answer = long.Parse(new string(a));
+            return answer;
+        }
+    }
+}
