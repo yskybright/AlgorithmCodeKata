a9b30ce6b1d9fccbc8e36e9beed49bbb4daab153
yskybright <rgh99@naver.com>
2023-12-01 09:58:03 +0900

    23.12.01

---
+++ _021_HarshadNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _021_HarshadNum
+    {
+        public bool solution(int x)
+        {
+            int div = 0;
+            int x2 = x;
+            while (x2 > 0)
+            {
+                div += x2 % 10;
+                x2 = x2 / 10;
+            }
+
+            if (x % div == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
