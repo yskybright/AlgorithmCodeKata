2b72a71244e150a61e7e8b1ac8277a566cb48ce1
yskybright <rgh99@naver.com>
2023-12-04 09:56:38 +0900

    23.12.04

---
+++ _022_SumOfInts.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _022_SumOfInts
+    {
+        public long solution(int a, int b)
+        {
+            long answer = 0;
+            while (a != b)
+            {
+                answer += a;
+                a = (a > b) ? a - 1 : a + 1;
+            }
+
+            return answer + b;
+        }
+    }
+}
---
+++ _023_Collatz.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _023_Collatz
+    {
+        public int solution(int num)
+        {
+            int answer = 0;
+            long n = num;
+            while (n != 1)
+            {
+                answer++;
+                n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
+            }
+            if (answer >= 500) answer = -1;
+            return answer;
+        }
+    }
+}
+
