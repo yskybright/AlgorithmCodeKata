b3d169f6d851814d7f08705c3217a1856ba33c58
yskybright <rgh99@naver.com>
2023-12-22 15:05:08 +0900

    23.12.22

---
+++ _034_ReverseStrings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    // 문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요.
+    // s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.
+    internal class _034_ReverseStrings
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+            char[] tmp = s.ToCharArray();
+
+            Array.Sort(tmp);
+            Array.Reverse(tmp);
+
+            answer = new string(tmp);
+
+            return answer;
+        }
+    }
+}
