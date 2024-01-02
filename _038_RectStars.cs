557965fdcf4d671059260fd33580be92ce162c24
yskybright <rgh99@naver.com>
2024-01-02 09:44:50 +0900

    24.01.02

---
+++ _038_RectStars.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _038_RectStars
+    {
+        // 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+        // 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+        public class Example
+        {
+            public static void Main()
+            {
+                String[] s;
+                Console.Clear();
+                s = Console.ReadLine().Split(' ');
+                int n = Int32.Parse(s[0]);
+                int m = Int32.Parse(s[1]);
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
---
+++ _039_GCDandLCM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _039_GCDandLCM
+    {
+        // 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요.
+        // 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다.
+        // 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+
+        public int[] solution(int n, int m)
+        {
+            int[] answer = new int[2];
+            int cdg; //최대공약수
+            int csg; //최소공배수
+
+            cdg = gcd(n, m);
+            csg = (n * m) / cdg;
+
+            answer[0] = cdg;
+            answer[1] = csg;
+            return answer;
+        }
+
+        // gcd = greatest common divisor
+        public int gcd(int n, int m)
+        {
+            if (m == 0) return n;
+            else return gcd(m, n % m);
+        }
+    }
+}
