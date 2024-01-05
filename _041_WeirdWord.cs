3d838529336f97906b8cf3980696e3ad61a79a80
yskybright <rgh99@naver.com>
2024-01-05 09:57:17 +0900

    24.01.05

---
+++ _041_WeirdWord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _041_WeirdWord
+    {
+        public string solution(string s)
+        {
+            string answer = "";
+            bool isEven = true;
+            for(int i=0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c != ' ')
+                {
+                    if (isEven)
+                    {
+                        answer += s[i].ToString().ToUpper();
+                        isEven = false;
+                    }
+                    else
+                    {
+                        answer += s[i].ToString().ToLower();
+                        isEven = true;
+                    }
+                }
+                else
+                {
+                    isEven = true;
+                    answer += " ";
+                }
+            }
+            return answer;
+        }
+    }
+}
+
