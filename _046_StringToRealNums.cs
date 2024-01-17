a28eb08cf2c6bdc8f6a35432e3a423f47008b50c
yskybright <rgh99@naver.com>
2024-01-17 09:05:26 +0900

    24.01.17
    숫자 문자열과 영단어 (Programmers)

---
+++ _046_StringToRealNums.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AlgorithmCodeKata
+{
+    internal class _046_StringToRealNums
+    {
+        // 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다.
+        // s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요. 
+        public class Solution
+        {
+            public int solution(string s)
+            {
+                string[] num = new string[]
+                {
+            "zero",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+                };
+
+                for (int i = 0; i < 10; i++)
+                {
+                    s = s.Replace(num[i], i.ToString());
+                }
+
+                return Int32.Parse(s);
+            }
+        }
+    }
+}
